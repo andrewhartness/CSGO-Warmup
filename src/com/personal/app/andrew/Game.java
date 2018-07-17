44c97642d348e4f040cc63fbdd49fce7efeaa2d0
Andrew Hartness <hartness.andrew@gmail.com>
2018-07-17 11:56:13 -0400

    Initial commit

---
+++ src/com/personal/app/andrew/Game.java
@@ -0,0 +1,18 @@
+package com.personal.app.andrew;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable{
+
+	public void start() {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	public static void main(String args[]) {
+		
+	}
+}
